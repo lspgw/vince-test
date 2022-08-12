ef4083580a6ee94b32b58a0f4fed6c6b11e2c278
lspgw <48206213+lspgw@users.noreply.github.com>
2022-08-12 14:20:36 +0800

    Add files via upload

---
+++ HelloWorld/src/main/HelloWorld.java
@@ -0,0 +1,18 @@
+package main;
+
+import org.junit.Test;
+
+public class HelloWorld {
+	//System.out.println("Hello World, I am Ian Xie.")
+	
+	@Test
+	public void main() {
+		System.out.println("Hello World, I am Ian Xie.");
+	}
+	
+	@Test
+	public void test() {
+		System.out.println("Hello World, I am Vince").
+	}
+}
+
