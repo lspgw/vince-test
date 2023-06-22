46968bf256dddcb79d7bf4ed990231e16a0e58cc
lspgw <48206213+lspgw@users.noreply.github.com>
2023-06-22 11:34:55 +0800

    Update HelloWorld.java

--- HelloWorld/src/main/HelloWorld.java
+++ HelloWorld/src/main/HelloWorld.java
@@ -4,7 +4,8 @@ import org.junit.Test;
 
 public class HelloWorld {
 	//System.out.println("Hello World, I am Ian Xie.")
-	
+
+	//System.out.println("hello, it's a mirror batch version.");
 	@Test
 	public void main() {
 		System.out.println("Hello World, I am Ian Xie.");
